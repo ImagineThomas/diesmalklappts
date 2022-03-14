cd6f0b1b9467aeb99ab2df827e2bf65b377c2d98
ImagineThomas <schmidt.thomas@hotmail.com>
2022-03-14 14:03:15 +0100

    SearchEMail

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -4,7 +4,9 @@ import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 import { AlertController, LoadingController } from '@ionic/angular';
 import { AuthService } from '../services/auth.service';
 import { ProfilePictureService } from '../services/profile-picture.service';
- 
+import { collection, query, where, getDocs } from "firebase/firestore";
+import { doc, docData, Firestore, setDoc } from '@angular/fire/firestore';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -12,24 +14,35 @@ import { ProfilePictureService } from '../services/profile-picture.service';
 })
 export class HomePage {
   profile = null;
- 
+  email: string = 'email';
   constructor(
     private profilePictureService: ProfilePictureService,
     private authService: AuthService,
     private router: Router,
     private loadingController: LoadingController,
-    private alertController: AlertController
+    private alertController: AlertController,
+    private firestore: Firestore
   ) {
     this.profilePictureService.getUserProfile().subscribe((data) => {
       this.profile = data;
     });
   }
- 
+
   async logout() {
     await this.authService.logout();
     this.router.navigateByUrl('/', { replaceUrl: true });
   }
- 
+
+  async findUserWithMail() {
+    const q = query(collection(this.firestore, "users"), where("email", "==", this.email));
+
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      // doc.data() is never undefined for query doc snapshots
+      console.log(doc.id, " => ", doc.data());
+    });
+  }
+  
   async changeImage() {
     const image = await Camera.getPhoto({
       quality: 90,
@@ -37,14 +50,14 @@ export class HomePage {
       resultType: CameraResultType.Base64,
       source: CameraSource.Photos, // Camera, Photos or Prompt!
     });
- 
+
     if (image) {
       const loading = await this.loadingController.create();
       await loading.present();
- 
+
       const result = await this.profilePictureService.uploadImage(image);
       loading.dismiss();
- 
+
       if (!result) {
         const alert = await this.alertController.create({
           header: 'Upload failed',
