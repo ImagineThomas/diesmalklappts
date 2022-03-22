ac275421df7f45b0102b71933d4ed351435caea8
PriiNcEHomeR <74197279+PriiNcEHomeR@users.noreply.github.com>
2022-03-22 21:47:54 +0100

    add profile.id to message

--- src/app/chat/chat.page.ts
+++ src/app/chat/chat.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Firestore, doc, setDoc} from '@angular/fire/firestore';
+import { ProfilePictureService } from '../services/profile-picture.service';
 
 @Component({
   selector: 'app-chat',
@@ -10,13 +11,19 @@ import { Firestore, doc, setDoc} from '@angular/fire/firestore';
 export class ChatPage implements OnInit {
 
   chatId: string;
+  profile = null;
   message: string;
 
   constructor(
     private router: Router,
     private route: ActivatedRoute,
     private firestore: Firestore,
-  ) { }
+    private profilePictureService: ProfilePictureService,
+  ) { 
+    this.profilePictureService.getUserProfile().subscribe((data) => {
+    this.profile = data;
+    });
+  }
 
   ngOnInit() {
     //erhält die ChatId aus der URL bei Chatinitialisierung
@@ -29,12 +36,12 @@ export class ChatPage implements OnInit {
 
   async sendMessage(){
     //der timestamp isn bissl strange aber scheint zu funktionieren/ reicht zum ordnen der Nachrichten -> Nachrichten werden eh immer unten hinzugefügt -> nicht neu geordnet
-    //falls man die Zeit cooler angeben will -> andere Funktio nutzen 
+    //falls man die Zeit cooler angeben will -> andere Funktion nutzen 
     const current = new Date();
     const timestamp = current.getTime();
     const chatDocRef = doc(this.firestore, `chats/${this.chatId}`);
     await setDoc(chatDocRef, {
-      [timestamp]: this.message
+      [timestamp]: this.profile.id + ":" + this.message
     },
       { merge: true });
 
